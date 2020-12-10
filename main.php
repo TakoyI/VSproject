3b07ed365bae4241f6029ec7598ac6fed88d4ebf
Noah Black <bagdadstyle@mail.ru>
2020-12-10 13:25:52 +0300

    commit

--- main.php
+++ main.php
@@ -8,7 +8,24 @@
     <title>Hello, world!</title>
   </head>
   <body>
-<header></header>
+<header class="container-fluid row">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand"><img src="images/" alt=""></a>
+    <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="my-nav" class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Item 1 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Item 2</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
 
 
 
